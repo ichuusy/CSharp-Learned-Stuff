97ceabf75e37a629b265067dd68c2c75b0ea43ae
ichuusy <83366765+ichuusy@users.noreply.github.com>
2022-08-11 16:03:56 +0300

    Add files via upload

---
+++ Interface-Abstract/CoffeeShop.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer("ichuusy","durden",31);
+            StarbucksCustomerService starbucksCustomerService = new StarbucksCustomerService(new CustomerCheckService());
+            starbucksCustomerService.Save(customer);
+            starbucksCustomerService.BuyCoffee();
+            Console.WriteLine();
+            NeroCustomerService neroCustomerService = new NeroCustomerService();
+            neroCustomerService.Save(customer);
+            neroCustomerService.BuyCoffee();
+            Console.ReadKey();
+        }
+
+        public class Customer 
+        {
+            public string firstname;
+            public string lastname;
+            public int age;
+            public Customer(string firstname, string lastname, int age) 
+            {
+                this.firstname = firstname;
+                this.lastname = lastname;
+                this.age = age;
+            }
+        }
+        interface ICustomerService 
+        {
+            void Save(Customer customer);
+        }
+
+        public abstract class BaseCustomerService : ICustomerService
+        {
+            public virtual void Save(Customer customer) 
+            {
+                Console.WriteLine("Informations Saved to database.");
+            }
+            public abstract void BuyCoffee();
+        }
+
+        public interface ICustomerCheckService 
+        {
+            bool CheckIfRealPerson(Customer customer);
+        }
+
+        public class CustomerCheckService : ICustomerCheckService 
+        {
+            public bool CheckIfRealPerson(Customer customer)
+            {
+                if(customer.firstname == "ichuusy" && customer.lastname == "durden") 
+                {
+                    Console.WriteLine("Information is correct.");
+                    return true;
+
+                }
+                else 
+                {
+                    throw new Exception("Information is not correct.");
+                }
+            }
+        }
+
+        public class StarbucksCustomerService : BaseCustomerService 
+        {
+            ICustomerCheckService _customerCheckService;
+
+            public StarbucksCustomerService(ICustomerCheckService customerCheckService)
+            {
+                _customerCheckService = customerCheckService;
+            }
+
+            public override void Save(Customer customer) 
+            {
+                if (_customerCheckService.CheckIfRealPerson(customer)) 
+                {
+                    Console.Write("Starbucks : ");
+                    base.Save(customer);
+                }
+            }
+            public override void BuyCoffee() 
+            {
+                Console.WriteLine("Starbucks : You earned a star thanks for buying coffee.");
+            }
+        }
+        public class NeroCustomerService : BaseCustomerService 
+        {
+            public override void Save(Customer customer)
+            {
+                base.Save(customer);
+            }
+            public override void BuyCoffee()
+            {
+                Console.WriteLine("Nero : Thanks for buying coffee.");
+            }
+        }
+    }
+}
---
+++ Interface-Abstract/Simple-ATM-Software.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_ATM_Software
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Bank bank = new Bank();
+            bank.Login(person);
+            bank.Withdraw(person, 300);
+            Console.WriteLine($"Person's money : {person.money}");
+            bank.Deposit(person, 100);
+            Console.WriteLine($"Person's money : {person.money}");
+            Console.Read();
+        }
+
+        public struct Person 
+        {
+            public string name = "ichuusy";
+            public string cardId = "123456789";
+            public int money = 500;
+        }
+        public abstract class LoginChecker : ILogin
+        {
+            public void Login(Person person)
+            {
+                if (person.name == "ichuusy" && person.cardId == "123456789")
+                {
+                    Console.WriteLine("Login successfully");
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+        }
+
+        interface ILogin
+        {
+            void Login(Person person);
+        }
+
+        public class Bank : LoginChecker
+        {
+            public void Withdraw(Person person, int withdrawedMoney)
+            {
+                if (person.money >= withdrawedMoney)
+                {
+                    person.money -= withdrawedMoney;
+                    Console.WriteLine("Money is withdrawed.");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, your money not enough.");
+                }
+            }
+            public void Deposit(Person person, int depositedMoney)
+            {
+                person.money -= depositedMoney;
+                Console.WriteLine("Money is deposited");
+            }
+        }
+
+    }
+}
